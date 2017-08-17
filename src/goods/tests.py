eb2d9c547dcad596464a20dfb00ebe318f1e5dc5
Valeriy Osipov <info@vosipov.com>
2017-08-17 23:10:44 +0300

    Add unit tests

--- src/goods/tests.py
+++ src/goods/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from goods.models import Category, Good
 
-# Create your tests here.
+
+class CategoryTests(TestCase):
+    def test_str(self):
+        c = Category(name='MyName', description='MyDescription')
+        self.assertEqual(str(c), 'MyName')
+
+
+class GoodTests(TestCase):
+    def test_str(self):
+        g = Good(name='MyName', description='MyDescription', in_stock=True)
+        self.assertEqual(str(g), 'MyName')
--- src/profiles/tests.py
+++ src/profiles/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from profiles.models import UserProfile, userStripe
 
-# Create your tests here.
+
+# class UserProfileTests(TestCase):
+#     def test_str(self):
+#         u = UserProfile(name='MyName', description='MyDescription')
+#         self.assertEqual(str(u), 'MyName')
+
+
+class userStripeTests(TestCase):
+    def test_str(self):
+        u = userStripe(stripe_id='MyStripeId')
+        self.assertEqual(str(u), 'MyStripeId')
