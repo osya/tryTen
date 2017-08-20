2873ff027069b89e77703f7c8358127bb0291916
Valeriy Osipov <info@vosipov.com>
2017-08-20 18:46:52 +0300

    Add create Good test

--- src/goods/tests.py
+++ src/goods/tests.py
@@ -1,14 +1,116 @@
-from django.test import TestCase
+import random
+import string
+
+import factory
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client, RequestFactory, LiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.ui import Select
+
 from goods.models import Category, Good
+from goods.views import GoodList
+
+
+def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+    username = factory.Sequence(lambda n: "Agent %03d" % n)
+    email = factory.LazyAttributeSequence(lambda o, n: f'{o.username}{n}@example.com')
+    password = factory.PostGenerationMethodCall('set_password')
+
+
+class CategoryFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = 'MyCategoryName'
+    description = 'MyCategoryDescription'
+
+
+class GoodFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Good
+
+    name = 'MyGoodName'
+    description = 'MyDescription'
+    category = factory.SubFactory(CategoryFactory)
+    in_stock = True
+    price = 100.0
 
 
 class CategoryTests(TestCase):
     def test_str(self):
-        c = Category(name='MyName', description='MyDescription')
-        self.assertEqual(str(c), 'MyName')
+        category = CategoryFactory()
+        self.assertEqual(str(category), 'MyCategoryName')
 
 
 class GoodTests(TestCase):
     def test_str(self):
-        g = Good(name='MyName', description='MyDescription', in_stock=True)
-        self.assertEqual(str(g), 'MyName')
+        good = GoodFactory()
+        self.assertEqual(str(good), 'MyGoodName')
+
+
+class GoodListViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = UserFactory(password=random_string_generator())
+
+    def test_no_goods_in_context(self):
+        request = self.factory.get('/')
+        request.user = self.user
+        response = GoodList.as_view()(request)
+        self.assertEquals(list(response.context_data['object_list']), [],)
+
+    def test_goods_in_context(self):
+        request = self.factory.get('/')
+        good = GoodFactory()
+        request.user = self.user
+        response = GoodList.as_view()(request)
+        self.assertEquals(list(response.context_data['object_list']), [good],)
+
+
+class CreatePostIntegrationTest(LiveServerTestCase):
+    selenium = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        cls.password = random_string_generator()
+        cls.user = UserFactory(password=cls.password)
+        cls.client = Client()
+        super(CreatePostIntegrationTest, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(CreatePostIntegrationTest, cls).tearDownClass()
+
+    def test_create_good(self):
+        self.assertTrue(self.client.login(username=self.user.username, password=self.password))
+        cookie = self.client.cookies[settings.SESSION_COOKIE_NAME]
+        # Replace `localhost` to 127.0.0.1 due to the WinError 10054 according to the
+        # https://stackoverflow.com/a/14491845/1360307
+        self.selenium.get(f'{self.live_server_url}{reverse("goods:create")}'.replace('localhost', '127.0.0.1'))
+        if cookie:
+            self.selenium.add_cookie({
+                'name': settings.SESSION_COOKIE_NAME,
+                'value': cookie.value,
+                'secure': False,
+                'path': '/'})
+        CategoryFactory()
+        self.selenium.refresh()  # need to update page for logged in user
+        self.selenium.find_element_by_id('id_name').send_keys('MyGoodName')
+        self.selenium.find_element_by_id('id_description').send_keys('MyDescription')
+        select = Select(self.selenium.find_element_by_id('id_category'))
+        all_options = [o.get_attribute('value') for o in select.options]
+        select.select_by_value(all_options[1])
+        self.selenium.find_element_by_id('id_price').send_keys('100.0')
+        self.selenium.find_element_by_xpath('//input[@type="submit"]').click()
+        self.assertEqual(Good.objects.first().name, 'MyGoodName')
--- src/profiles/tests.py
+++ src/profiles/tests.py
@@ -1,14 +1,52 @@
+import random
+import string
+
+import factory
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+
 from profiles.models import UserProfile, userStripe
 
 
-# class UserProfileTests(TestCase):
-#     def test_str(self):
-#         u = UserProfile(name='MyName', description='MyDescription')
-#         self.assertEqual(str(u), 'MyName')
+def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+    username = factory.Sequence(lambda n: "Agent %03d" % n)
+    email = factory.LazyAttributeSequence(lambda o, n: f'{o.username}{n}@example.com')
+    password = factory.PostGenerationMethodCall('set_password')
+
+
+class UserProfileFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = UserProfile
+
+    user = factory.SubFactory(UserFactory, password=random_string_generator())
+    description = 'MyDescription'
+    city = 'MyCity'
+    website = 'MyWebsite'
+    phone_number = 123456
+
+
+class UserStripeFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = userStripe
+
+    user = factory.SubFactory(UserFactory, password=random_string_generator())
+    stripe_id = 'MyStripeId'
+
+
+class UserProfileTests(TestCase):
+    def test_str(self):
+        user_profile = UserProfileFactory()
+        self.assertEqual(str(user_profile), user_profile.user.username)
 
 
 class userStripeTests(TestCase):
     def test_str(self):
-        u = userStripe(stripe_id='MyStripeId')
-        self.assertEqual(str(u), 'MyStripeId')
+        user_stripe = UserStripeFactory()
+        self.assertEqual(str(user_stripe), 'MyStripeId')
