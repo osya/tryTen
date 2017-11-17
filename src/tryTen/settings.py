39f1373229d14384135d41158a1d8503ce4fa74b
Valeriy Osipov <info@vosipov.com>
2017-11-17 23:20:01 +0300

    Add default value for ALLOWED_HOSTS

--- src/tryTen/settings.py
+++ src/tryTen/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
