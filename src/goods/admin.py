767707eaa5932a1f1cad3c7e6db873f0a428a133
Valeriy Osipov <info@vosipov.com>
2018-02-28 13:15:30 +0300

    Fix slug in Admin panel

--- src/goods/admin.py
+++ src/goods/admin.py
@@ -2,10 +2,4 @@ from django.contrib import admin
 
 from goods.models import Good
 
-
-class GoodAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('category', 'name')}
-    # TODO: Make prepopulated_fields somehow fill `slug` field based on (category.name, name)
-
-
-admin.site.register(Good, GoodAdmin)
+admin.site.register(Good)
